2f2dc00066bb06e2efae8e72d17e00dfce7dbae3
SasmithaHiram <sasmithahiramm2003@gmail.com>
2026-01-05 12:05:44 +0530

    models and db context

---
+++ Controllers/Models/Quote.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Controllers.Models
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public string Author { get; set; }
+        public string Category { get; set; }
+        public string Emoji { get; set; }
+    }
+}
---
+++ Data/QuoteDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Controllers.Models;
+
+namespace WebApplication1.Data
+{
+    public class QuoteDbContext : DbContext
+    {
+        public QuoteDbContext(DbContextOptions<QuoteDbContext> options) : base(options) { }
+
+        public DbSet<Quote> Quotes { get; set; }
+
+    }
+}
